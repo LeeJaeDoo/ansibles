16f21267175e3ae65390b23c54b63fdbb4b7783d
Jaedoo.Lee <dlwoen9@godo.co.kr>
2021-03-10 23:52:06 +0900

    modify start.sh

--- roles/test/files/start.sh
+++ roles/test/files/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ssh -i sp-member.pem ubuntu@15.165.213.153 "rm ~/app/sp-0.0.1-SNAPSHOT.jar"
+ssh -i ~/ansibles/roles/test/files/sp-member.pem ubuntu@15.165.213.153 "rm ~/app/sp-0.0.1-SNAPSHOT.jar ~/app/stop.sh"
 
-scp -i sp-member.pem /var/lib/jenkins/workspace/test/build/libs/sp-0.0.1-SNAPSHOT.jar ubuntu@15.165.213.153:~/app
+scp -i ~/ansibles/roles/test/files/sp-member.pem /var/lib/jenkins/workspace/test/build/libs/sp-0.0.1-SNAPSHOT.jar ~/ansibles/roles/test/files/stop.sh ubuntu@15.165.213.153:~/app
 
-ssh -i sp-member.pem ubuntu@15.165.213.153 "nohup java -jar ~/app/sp-0.0.1-SNAPSHOT.jar"
+ssh -i ~/ansibles/roles/test/files/sp-member.pem ubuntu@15.165.213.153 "nohup java -jar ~/app/sp-0.0.1-SNAPSHOT.jar"
