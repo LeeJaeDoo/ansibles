fc7bae410d471f84e91c343b83e82af77d8719c4
Jaedoo.Lee <dlwoen9@godo.co.kr>
2021-03-11 00:36:06 +0900

    modify keyfile path

--- roles/test/files/start.sh
+++ roles/test/files/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ssh -i ~/ansibles/roles/test/files/sp-member.pem ubuntu@15.165.213.153 "rm ~/app/sp-0.0.1-SNAPSHOT.jar ~/app/stop.sh"
+ssh -i /home/ubuntu/ansibles/roles/test/files/sp-member.pem ubuntu@15.165.213.153 "rm ~/app/sp-0.0.1-SNAPSHOT.jar ~/app/stop.sh"
 
-scp -i ~/ansibles/roles/test/files/sp-member.pem /var/lib/jenkins/workspace/test/build/libs/sp-0.0.1-SNAPSHOT.jar ~/ansibles/roles/test/files/stop.sh ubuntu@15.165.213.153:~/app
+scp -i /home/ubuntu/ansibles/roles/test/files/sp-member.pem /var/lib/jenkins/workspace/test/build/libs/sp-0.0.1-SNAPSHOT.jar ~/ansibles/roles/test/files/stop.sh ubuntu@15.165.213.153:~/app
 
-ssh -i ~/ansibles/roles/test/files/sp-member.pem ubuntu@15.165.213.153 "nohup java -jar ~/app/sp-0.0.1-SNAPSHOT.jar"
+ssh -i /home/ubuntu/ansibles/roles/test/files/sp-member.pem ubuntu@15.165.213.153 "nohup java -jar ~/app/sp-0.0.1-SNAPSHOT.jar"
